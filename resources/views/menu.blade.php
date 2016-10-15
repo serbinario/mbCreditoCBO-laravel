fcb7e59db81651a0169e627a3ef5abe3cc574046
Paulo Vaz <paulo@serbinario.com.br>
2016-10-14 23:37:01 -0300

    Instalação de ng-resource

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -74,6 +74,8 @@
 <script type="text/javascript" src={{ asset('/lib/materialize/dist/js/materialize.js') }}></script>
 <script type="text/javascript" src={{ asset('/lib/angular/angular.js') }}></script>
 <script type="text/javascript" src={{ asset('/lib/datatables/media/js/jquery.dataTables.js') }}></script>
+<script type="text/javascript" src={{ asset('/lib/angular-resource/angular-resource.js') }}></script>
+
 <!--AngularJS datatables-->
 {{--<script type="text/javascript" src={{ asset('/lib/datatables.net/js/jquery.dataTables.js') }}></script>--}}
 <script type="text/javascript" src={{ asset('/app.js') }}></script>
