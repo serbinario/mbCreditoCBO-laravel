0ecbb52de781f940929f1ffc18298868bf179243
Paulo Vaz <paulo@serbinario.com.br>
2016-10-21 14:50:52 -0300

    Adicionado css datatables.materializer

--- resources/views/agencia/index.blade.php
+++ resources/views/agencia/index.blade.php
@@ -4,7 +4,7 @@
 
     <div class="container">
         <div class="col m6 s12">
-            <div class="card-panel">
+            <div class="card material-table" style="margin: 20px auto; padding:20px;">
                 <h3 class="box-title">Consultar Agente</h3>
                 <!-- inicio botao -->
                 <div class="row">
@@ -15,9 +15,18 @@
                     </div>
                 </div>
                 <!-- fim botao -->
+
+                <div class="table-header">
+                    <span class="table-title">Material Datatable</span>
+                    <div class="actions">
+                        <a href="#add_users" class="modal-trigger waves-effect btn-flat nopadding"><i class="material-icons">person_add</i></a>
+                        <a href="#" class="search-toggle waves-effect btn-flat nopadding"><i class="material-icons">search</i></a>
+                    </div>
+                </div>
+
                 <div class="row">
                     <div class="col-md-12">
-                        <table id="agencia-grid" class="display table table-bordered" cellspacing="0" width="100%">
+                        <table id="agencia-grid" cellspacing="0" width="100%">
                             <thead>
                             <tr>
                                 <th>Número da Agência</th>
@@ -25,13 +34,13 @@
                                 <th>Açao</th>
                             </tr>
                             </thead>
-                            <tfoot>
+                            <tbody>
                             <tr>
                                 <th>Número da Agência</th>
                                 <th>Nome da Agência</th>
                                 <th>Açao</th>
                             </tr>
-                            </tfoot>
+                            <tbody>
                         </table>
                     </div>
                 </div>
@@ -50,7 +59,21 @@
                 {data: 'numero_agencia', name: 'agencias_callcenter.numero_agencia'},
                 {data: 'nome_agencia', name: 'agencias_callcenter.nome_agencia'},
                 {data: 'action', name: 'action', orderable: false, searchable: false}
-            ]
+            ],
+            "oLanguage": {
+                "sStripClasses": "",
+                "sSearch": "",
+                "sSearchPlaceholder": "Enter Keywords Here",
+                "sInfo": "_START_ - _END_ de _TOTAL_",
+                "sLengthMenu": '<span>Linhas por Página:</span><select class="browser-default">' +
+                '<option value="10">10</option>' +
+                '<option value="20">20</option>' +
+                '<option value="30">30</option>' +
+                '<option value="40">40</option>' +
+                '<option value="50">50</option>' +
+                '<option value="-1">All</option>' +
+                '</select></div>'
+            },
         });
     </script>
 @stop
--- resources/views/contrato/index.blade.php
+++ resources/views/contrato/index.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container">
     <div class="col m6 s12">
-        <div class="card-panel">
+        <div class="card material-table" style="margin: 20px auto; padding:20px;">
             <h3 class="box-title">Consultar Agente</h3>
             <!-- inicio botao -->
             <div class="row">
@@ -15,6 +15,15 @@
                 </div>
             </div>
             <!-- fim botao -->
+
+            <div class="table-header">
+                <span class="table-title">Material Datatable</span>
+                <div class="actions">
+                    <a href="#add_users" class="modal-trigger waves-effect btn-flat nopadding"><i class="material-icons">person_add</i></a>
+                    <a href="#" class="search-toggle waves-effect btn-flat nopadding"><i class="material-icons">search</i></a>
+                </div>
+            </div>
+
             <div class="row">
                 <div class="col-md-12">
                     <table id="contrato-grid" class="display table table-bordered" cellspacing="0" width="100%">
@@ -61,7 +70,21 @@
                 {data: 'conta', name: 'clientes.conta'},
                 {data: 'telefone', name: 'telefones.telefone'},
                 {data: 'action', name: 'action', orderable: false, searchable: false}
-            ]
+            ],
+            "oLanguage": {
+                "sStripClasses": "",
+                "sSearch": "",
+                "sSearchPlaceholder": "Enter Keywords Here",
+                "sInfo": "_START_ - _END_ de _TOTAL_",
+                "sLengthMenu": '<span>Linhas por Página:</span><select class="browser-default">' +
+                '<option value="10">10</option>' +
+                '<option value="20">20</option>' +
+                '<option value="30">30</option>' +
+                '<option value="40">40</option>' +
+                '<option value="50">50</option>' +
+                '<option value="-1">All</option>' +
+                '</select></div>'
+            },
         });
     </script>
 @stop
