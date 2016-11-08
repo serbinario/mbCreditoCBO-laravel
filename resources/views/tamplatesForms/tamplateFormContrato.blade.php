7cb4b8b234cb56827f77ab0d0c94fbbf1e9efcf8
Paulo Vaz <paulo@serbinario.com.br>
2016-11-08 08:24:52 -0300

    Alterado Contratos Template

--- resources/views/tamplatesForms/tamplateFormContrato.blade.php
+++ resources/views/tamplatesForms/tamplateFormContrato.blade.php
@@ -1,31 +1,52 @@
+<style>
+    .topo-conteudo-full {
+        border-bottom: 1px solid #c3ced2;
+        color: #136d9c;
+        display: block;
+        margin-bottom: 10px;
+        text-transform: uppercase;
+    }
+</style>
+
+<div class="block-header">
+    <h2>Cadastro de Contratos</h2>
+
+</div>
+
 <div class="card">
-    <div class="card-header">
-    </div>
-
     <div class="card-body card-padding">
-
-        <div class="form-group">
-            <div class="fg-line">
-                <label for="searchCpf">Consultar CPF</label>
-                {!! Form::text('searchCpf', Session::getOldInput('searchCpf'), array('id' => 'searchCliente', 'class' => 'form-control input-sm', 'placeholder' => 'Número de CPF')) !!}
-            </div>
+        <div class="topo-conteudo-full">
+            <h4>Consultar cliente por CPF:</h4>
         </div>
 
-        <button type="button" id="btnConsultar">Consultar</button>
-        </br>
-        {{--<hr>--}}
+        <div class="row">
+                <div class="form-group col-sm-4">
+                    <div class=" fg-line">
+                        <label for="searchCpf"></label>
+                        {!! Form::text('searchCpf', Session::getOldInput('searchCpf'), array('id' => 'searchCliente', 'class' => 'form-control input-sm', 'placeholder' => 'Número de CPF')) !!}
+                    </div>
+                </div>
+            <div class="col-sm-4 m-t-15">
+                <a class="btn btn-primary btn-sm m-t-10" id="btnConsultar" href="#">Consultar</a>
 
-        <div class="form-group">
-            <div class=" fg-line">
-                <label for="cliente[name]">Nome </label>
-                {!! Form::text('cliente[name]', Session::getOldInput('cliente[name]'), array('id' => 'clienteNome', 'class' => 'form-control input-sm', 'placeholder' => 'Nome completo do cliente')) !!}
             </div>
         </div>
+        <div class="topo-conteudo-full">
+            <h4>Dados do cliente</h4>
+        </div>
 
-        <div class="form-group">
-            <div class=" fg-line">
-                <label for="cliente[cpf]">CPF</label>
-                {!! Form::text('cliente[cpf]', Session::getOldInput('clientes[cpf]'), array('id' => 'clienteCpf', 'class' => 'form-control input-sm', 'placeholder' => 'CPF do cliente')) !!}
+        <div class="row">
+            <div class="form-group col-sm-8">
+                <div class=" fg-line">
+                    <label for="cliente[name]">Nome </label>
+                    {!! Form::text('cliente[name]', Session::getOldInput('cliente[name]'), array('id' => 'clienteNome', 'class' => 'form-control input-sm', 'placeholder' => 'Nome completo do cliente')) !!}
+                    </div>
+                </div>
+            <div class="form-group col-sm-4">
+                <div class="fg-line">
+                    <label for="cliente[cpf]">CPF</label>
+                    {!! Form::text('cliente[cpf]', Session::getOldInput('clientes[cpf]'), array('id' => 'clienteCpf', 'class' => 'form-control input-sm', 'placeholder' => 'CPF do cliente')) !!}
+                </div>
             </div>
         </div>
 
@@ -40,74 +61,84 @@
             </div>
         </div>
 
-        <div class="form-group">
-            <div class="fg-line">
-                <label for="cliente[agencia_id]">Agência</label>
-                <div class="select">
-                   {!! Form::select('cliente[agencia_id]', ([$loadFields['agenciacallcenter']->toArray()]), null, array('class'=> 'form-control')) !!}
-                </div>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="fg-line">
-                <label for="cliente[conta]">Conta</label>
-                {!! Form::text('cliente[conta]', Session::getOldInput('cliente[conta]'), array('id' => 'clienteConta', 'class' => 'form-control input-sm', 'placeholder' => 'Conta do cliente')) !!}
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="fg-line">
-                <label for="convenio_id">Convêmio</label>
-                <div class="select">
-                    {!! Form::select('convenio_id', ([$loadFields['conveniocallcenter']->toArray()]), null, array('class'=> 'form-control')) !!}
-                </div>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="fg-line">
-                <label for="tipo_contrato_id">Tipos de Créditos</label>
-                <div class="select">
-                {!! Form::select('tipo_contrato_id', ([$loadFields['tipocontrato']->toArray()]), null, array('class'=> 'form-control')) !!}
-                </div>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="fg-line">
-                <label for="data_contratado">Data da Contratação</label>
-                {!! Form::text('data_contratado', Session::getOldInput('data_contratado'), array('class' => 'form-control input-sm', 'placeholder' => 'Data da contratação')) !!}
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="fg-line">
-                <label for="valor_contratado">Valor Contratado</label>
-                {!! Form::text('valor_contratado', Session::getOldInput('valor_contratado'), array('class' => 'form-control input-sm', 'placeholder' => 'Valor do Contrato')) !!}
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="fg-line">
-                <label for="codigo_transacao">Nº do Contrato</label>
-                {!! Form::text('codigo_transacao', Session::getOldInput('codigo_transacao'), array('class' => 'form-control input-sm', 'placeholder' => 'Número do Contrato')) !!}
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="fg-line">
-                <label for="matricula">Matrícula</label>
-                {!! Form::text('matricula', Session::getOldInput('matricula'), array('class' => 'form-control input-sm', 'placeholder' => 'Número do Contrato')) !!}
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="fg-line">
-                <label for="prazo">Quantidade de Parcelas</label>
-                <div class="form-group">
+        <div class="row">
+            <div class="form-group col-sm-4">
+                <div class=" fg-line">
+                    <label for="cliente[agencia_id]">Agência</label>
                     <div class="select">
-                    {!! Form::select('prazo', ([$loadFields['contrato']->toArray()]), null, array('class'=> 'form-control')) !!}
+                        {!! Form::select('cliente[agencia_id]', ([$loadFields['agenciacallcenter']->toArray()]), null, array('class'=> 'form-control')) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="form-group col-sm-4">
+                <div class="fg-line">
+                    <label for="cliente[agencia_id]">Agência</label>
+                    <div class="select">
+                        {!! Form::select('cliente[agencia_id]', ([$loadFields['agenciacallcenter']->toArray()]), null, array('class'=> 'form-control')) !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="topo-conteudo-full">
+            <h4>Dados do Contrato</h4>
+        </div>
+
+        <div class="row">
+            <div class="form-group col-sm-4">
+                <div class=" fg-line">
+                    <label for="convenio_id">Convêmio</label>
+                    <div class="select">
+                        {!! Form::select('convenio_id', ([$loadFields['conveniocallcenter']->toArray()]), null, array('class'=> 'form-control')) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="form-group col-sm-4">
+                <div class="fg-line">
+                    <label for="tipo_contrato_id">Tipos de Créditos</label>
+                    <div class="select">
+                        {!! Form::select('tipo_contrato_id', ([$loadFields['tipocontrato']->toArray()]), null, array('class'=> 'form-control')) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="form-group col-sm-4">
+                <div class="fg-line">
+                    <div class="fg-line">
+                        <label for="data_contratado">Data da Contratação</label>
+                        {!! Form::text('data_contratado', Session::getOldInput('data_contratado'), array('class' => 'form-control input-sm', 'placeholder' => 'Data da contratação')) !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="form-group col-sm-3">
+                <div class=" fg-line">
+                    <label for="valor_contratado">Valor Contratado</label>
+                    {!! Form::text('valor_contratado', Session::getOldInput('valor_contratado'), array('class' => 'form-control input-sm', 'placeholder' => 'Valor do Contrato')) !!}
+                </div>
+            </div>
+            <div class="form-group col-sm-3">
+                <div class="fg-line">
+                    <label for="codigo_transacao">Nº do Contrato</label>
+                    {!! Form::text('codigo_transacao', Session::getOldInput('codigo_transacao'), array('class' => 'form-control input-sm', 'placeholder' => 'Número do Contrato')) !!}
+                </div>
+            </div>
+            <div class="form-group col-sm-3">
+                <div class="fg-line">
+                    <label for="matricula">Matrícula</label>
+                    {!! Form::text('matricula', Session::getOldInput('matricula'), array('class' => 'form-control input-sm', 'placeholder' => 'Número do Contrato')) !!}
+                </div>
+            </div>
+
+            <div class="form-group col-sm-3">
+                <div class="fg-line">
+                    <label for="prazo">Quantidade de Parcelas</label>
+                    <div class="form-group">
+                        <div class="select">
+                            {!! Form::select('prazo', ([$loadFields['contrato']->toArray()]), null, array('class'=> 'form-control')) !!}
+                        </div>
                     </div>
                 </div>
             </div>
