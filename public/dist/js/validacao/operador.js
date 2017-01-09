8e0fb1c98f43d7db58ddfe89183e9704d940cdca
Felipe Lima <felipeldnascimento@gmail.com>
2017-01-09 11:16:04 +0000

    Alterações

--- public/dist/js/validacao/operador.js
+++ public/dist/js/validacao/operador.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
             cod_operadores: {
                 required: true,
                 chaveJ: true,
-                unique: ['http://mbCreditoCBO-laravel/public/index.php/operador/searchChaveJ', $('#idOperador')]
+                unique: ['/mbCreditoCBO-laravel/public/index.php/operador/searchChaveJ', $('#idOperador')]
             }
         },
         //For custom messages
