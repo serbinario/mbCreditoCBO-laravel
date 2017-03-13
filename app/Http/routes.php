cad3601e460905bb27ea25cf733da3397856dc63
Felipe Lima <felipeldnascimento@gmail.com>
2017-03-13 09:22:28 +0000

    Teste

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,87 +13,89 @@ Route::post('auth/register', 'Auth\AuthController@postRegister');
  *
  */
 Route::group(['middleware' => 'auth'], function () {
-    Route::group(['prefix' => '/'], function () {
-        Route::get('index', ['as' => 'index', 'uses' => 'DefaultController@index']);
-        Route::get('dashboard', ['as' => 'dashboard', 'uses' => 'DashBoardController@index']);
-        Route::get('dashboard/chartContratosByMonth/{searchAgente}', ['as' => 'dashboard.chartContratosByMonth', 'uses' => 'DashBoardController@getDataChartContratosByMonth']);
-        Route::get('dashboard/chartContratosByYear/{searchAgente}', ['as' => 'dashboard.chartContratosByYear', 'uses' => 'DashBoardController@getDataChartContratosByYear']);
 
-        Route::group(['prefix' => 'operador', 'as' => 'operador.'], function () {
-            Route::get('index', ['as' => 'index', 'uses' => 'OperadorController@index']);
-            Route::get('grid', ['as' => 'grid', 'uses' => 'OperadorController@grid']);
-            Route::get('create', ['as' => 'create', 'uses' => 'OperadorController@create']);
-            Route::post('store', ['as' => 'store', 'uses' => 'OperadorController@store']);
-            Route::get('edit/{id}', ['as' => 'edit', 'uses' => 'OperadorController@edit']);
-            Route::post('update/{id}', ['as' => 'update', 'uses' => 'OperadorController@update']);
+    Route::get('');
 
-            //unique chave j
-            Route::post('searchChaveJ', ['as' => 'searchChaveJ', 'uses' => 'OperadorController@searchChaveJ']);
-        });
+    Route::get('index', ['as' => 'index', 'uses' => 'DefaultController@index']);
+    Route::get('dashboard', ['as' => 'dashboard', 'uses' => 'DashBoardController@index']);
+    Route::get('dashboard/chartContratosByMonth/{searchAgente}', ['as' => 'dashboard.chartContratosByMonth', 'uses' => 'DashBoardController@getDataChartContratosByMonth']);
+    Route::get('dashboard/chartContratosByYear/{searchAgente}', ['as' => 'dashboard.chartContratosByYear', 'uses' => 'DashBoardController@getDataChartContratosByYear']);
 
-        Route::group(['prefix' => 'contrato', 'as' => 'contrato.'], function () {
-            Route::get('index', ['as' => 'index', 'uses' => 'ContratoController@index']);
-            Route::get('grid', ['as' => 'grid', 'uses' => 'ContratoController@grid']);
-            Route::get('create', ['as' => 'create', 'uses' => 'ContratoController@create']);
-            Route::get('createContrato/{idCliente}', ['as' => 'createContrato', 'uses' => 'ContratoController@createContrato']);
-            Route::post('store', ['as' => 'store', 'uses' => 'ContratoController@store']);
-            Route::get('edit/{id}', ['as' => 'edit', 'uses' => 'ContratoController@edit']);
-            Route::post('update/{id}', ['as' => 'update', 'uses' => 'ContratoController@update']);
+    Route::group(['prefix' => 'operador', 'as' => 'operador.'], function () {
+        Route::get('index', ['as' => 'index', 'uses' => 'OperadorController@index']);
+        Route::get('grid', ['as' => 'grid', 'uses' => 'OperadorController@grid']);
+        Route::get('create', ['as' => 'create', 'uses' => 'OperadorController@create']);
+        Route::post('store', ['as' => 'store', 'uses' => 'OperadorController@store']);
+        Route::get('edit/{id}', ['as' => 'edit', 'uses' => 'OperadorController@edit']);
+        Route::post('update/{id}', ['as' => 'update', 'uses' => 'OperadorController@update']);
 
-            //Busca de cliente por CPF em cadastro de contratos
-            Route::get('searchCliente/{clienteCpf}', ['as' => 'searchCliente', 'uses' => 'ContratoController@searchCliente']);
-
-            /*Rota que possibilita o teste de cadastro contratos
-            onde é feito o teste se o no. contrato que foi inserido
-            já se encontra cadastrado*/
-            Route::post('searchContrato', ['as' => 'searchContrato', 'uses' => 'ContratoController@searchContrato']);
-
-            /*Rota que possibilita o teste de cadastro contratos
-            onde é feito o teste se o cpf que foi inserido já se
-            encontra cadastrado*/
-            Route::post('searchCpf', ['as' => 'searchCpf', 'uses' => 'ContratoController@searchCpf']);
-
-            # Rotas de GERENCIAMENTO DE TELEFONES
-            Route::get('telefone/grid/{idClient}', ['as' => 'telefone.grid', 'uses' => 'ContratoController@gridPhones']);
-            Route::post('telefone/store/{idClient}', ['as' => 'telefone.create', 'uses' => 'ContratoController@storePhone']);
-
-            # Rota do pdf do contrato
-            Route::get('viewContrato/{id}', ['as' => 'viewContrato', 'uses' => 'ContratoController@viewContrato']);
-
-            #
-            Route::get('buscaAgencia', ['as' => 'buscaAgencia', 'uses' => 'ContratoController@buscaAgencia']);
-            Route::get('buscaAgencia/{idAgencia}', ['as' => 'buscaAgencia', 'uses' => 'ContratoController@buscaAgencia']);
-            Route::get('buscaNoAgencia/{idAgencia}', ['as' => 'buscaNoAgencia', 'uses' => 'ContratoController@buscaNoAgencia']);
-        });
-
-        Route::group(['prefix' => 'agencia', 'as' => 'agencia.'], function () {
-            Route::get('index', ['as' => 'index', 'uses' => 'AgenciaCallCenterController@index']);
-            Route::get('grid', ['as' => 'grid', 'uses' => 'AgenciaCallCenterController@grid']);
-            Route::get('create', ['as' => 'create', 'uses' => 'AgenciaCallCenterController@create']);
-            Route::post('store', ['as' => 'store', 'uses' => 'AgenciaCallCenterController@store']);
-            Route::get('edit/{id}', ['as' => 'edit', 'uses' => 'AgenciaCallCenterController@edit']);
-            Route::post('update/{id}', ['as' => 'update', 'uses' => 'AgenciaCallCenterController@update']);
-        });
-
-        Route::group(['prefix' => 'convenio', 'as' => 'convenio.'], function () {
-            Route::get('index', ['as' => 'index', 'uses' => 'ConvenioCallCenterController@index']);
-            Route::get('grid', ['as' => 'grid', 'uses' => 'ConvenioCallCenterController@grid']);
-            Route::get('create', ['as' => 'create', 'uses' => 'ConvenioCallCenterController@create']);
-            Route::post('store', ['as' => 'store', 'uses' => 'ConvenioCallCenterController@store']);
-            Route::get('edit/{id}', ['as' => 'edit', 'uses' => 'ConvenioCallCenterController@edit']);
-            Route::post('update/{id}', ['as' => 'update', 'uses' => 'ConvenioCallCenterController@update']);
-        });
-
-        Route::group(['prefix' => 'usuario', 'as' => 'usuario.'], function () {
-            Route::get('index', ['as' => 'index', 'uses' => 'UsuarioController@index']);
-            Route::get('grid', ['as' => 'grid', 'uses' => 'UsuarioController@grid']);
-            Route::get('create', ['as' => 'create', 'uses' => 'UsuarioController@create']);
-            Route::post('store', ['as' => 'store', 'uses' => 'UsuarioController@store']);
-            Route::get('edit/{id}', ['as' => 'edit', 'uses' => 'UsuarioController@edit']);
-            Route::post('update/{id}', ['as' => 'update', 'uses' => 'UsuarioController@update']);
-            Route::get('verificarOperador', ['as' => 'verificarOperador', 'uses' => 'UsuarioController@verificarOperador']);
-        });
+        //unique chave j
+        Route::post('searchChaveJ', ['as' => 'searchChaveJ', 'uses' => 'OperadorController@searchChaveJ']);
     });
+
+    Route::group(['prefix' => 'contrato', 'as' => 'contrato.'], function () {
+        Route::get('index', ['as' => 'index', 'uses' => 'ContratoController@index']);
+        Route::get('grid', ['as' => 'grid', 'uses' => 'ContratoController@grid']);
+        Route::get('create', ['as' => 'create', 'uses' => 'ContratoController@create']);
+        Route::get('createContrato/{idCliente}', ['as' => 'createContrato', 'uses' => 'ContratoController@createContrato']);
+        Route::post('store', ['as' => 'store', 'uses' => 'ContratoController@store']);
+        Route::get('edit/{id}', ['as' => 'edit', 'uses' => 'ContratoController@edit']);
+        Route::post('update/{id}', ['as' => 'update', 'uses' => 'ContratoController@update']);
+
+        //Busca de cliente por CPF em cadastro de contratos
+        Route::get('searchCliente/{clienteCpf}', ['as' => 'searchCliente', 'uses' => 'ContratoController@searchCliente']);
+
+        /*Rota que possibilita o teste de cadastro contratos
+        onde é feito o teste se o no. contrato que foi inserido
+        já se encontra cadastrado*/
+        Route::post('searchContrato', ['as' => 'searchContrato', 'uses' => 'ContratoController@searchContrato']);
+
+        /*Rota que possibilita o teste de cadastro contratos
+        onde é feito o teste se o cpf que foi inserido já se
+        encontra cadastrado*/
+        Route::post('searchCpf', ['as' => 'searchCpf', 'uses' => 'ContratoController@searchCpf']);
+
+        # Rotas de GERENCIAMENTO DE TELEFONES
+        Route::get('telefone/grid/{idClient}', ['as' => 'telefone.grid', 'uses' => 'ContratoController@gridPhones']);
+        Route::post('telefone/store/{idClient}', ['as' => 'telefone.create', 'uses' => 'ContratoController@storePhone']);
+
+        # Rota do pdf do contrato
+        Route::get('viewContrato/{id}', ['as' => 'viewContrato', 'uses' => 'ContratoController@viewContrato']);
+
+        #
+        Route::get('buscaAgencia', ['as' => 'buscaAgencia', 'uses' => 'ContratoController@buscaAgencia']);
+        Route::get('buscaAgencia/{idAgencia}', ['as' => 'buscaAgencia', 'uses' => 'ContratoController@buscaAgencia']);
+        Route::get('buscaNoAgencia/{idAgencia}', ['as' => 'buscaNoAgencia', 'uses' => 'ContratoController@buscaNoAgencia']);
+    });
+
+    Route::group(['prefix' => 'agencia', 'as' => 'agencia.'], function () {
+        Route::get('index', ['as' => 'index', 'uses' => 'AgenciaCallCenterController@index']);
+        Route::get('grid', ['as' => 'grid', 'uses' => 'AgenciaCallCenterController@grid']);
+        Route::get('create', ['as' => 'create', 'uses' => 'AgenciaCallCenterController@create']);
+        Route::post('store', ['as' => 'store', 'uses' => 'AgenciaCallCenterController@store']);
+        Route::get('edit/{id}', ['as' => 'edit', 'uses' => 'AgenciaCallCenterController@edit']);
+        Route::post('update/{id}', ['as' => 'update', 'uses' => 'AgenciaCallCenterController@update']);
+    });
+
+    Route::group(['prefix' => 'convenio', 'as' => 'convenio.'], function () {
+        Route::get('index', ['as' => 'index', 'uses' => 'ConvenioCallCenterController@index']);
+        Route::get('grid', ['as' => 'grid', 'uses' => 'ConvenioCallCenterController@grid']);
+        Route::get('create', ['as' => 'create', 'uses' => 'ConvenioCallCenterController@create']);
+        Route::post('store', ['as' => 'store', 'uses' => 'ConvenioCallCenterController@store']);
+        Route::get('edit/{id}', ['as' => 'edit', 'uses' => 'ConvenioCallCenterController@edit']);
+        Route::post('update/{id}', ['as' => 'update', 'uses' => 'ConvenioCallCenterController@update']);
+    });
+
+    Route::group(['prefix' => 'usuario', 'as' => 'usuario.'], function () {
+        Route::get('index', ['as' => 'index', 'uses' => 'UsuarioController@index']);
+        Route::get('grid', ['as' => 'grid', 'uses' => 'UsuarioController@grid']);
+        Route::get('create', ['as' => 'create', 'uses' => 'UsuarioController@create']);
+        Route::post('store', ['as' => 'store', 'uses' => 'UsuarioController@store']);
+        Route::get('edit/{id}', ['as' => 'edit', 'uses' => 'UsuarioController@edit']);
+        Route::post('update/{id}', ['as' => 'update', 'uses' => 'UsuarioController@update']);
+        Route::get('verificarOperador', ['as' => 'verificarOperador', 'uses' => 'UsuarioController@verificarOperador']);
+    });
+
 });
 
 //Rotas API restfull
