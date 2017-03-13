a023802c4ceb03877b5f1f9e1a85363cead8aee4
Felipe Lima <felipeldnascimento@gmail.com>
2017-03-13 13:28:49 +0000

    Alterações

---
+++ resources/views/auth/_login.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<!--[if IE 9 ]><html class="ie9"><![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Material Admin</title>
+
+    <!--CSS Vendor-->
+    <link type="text/css" rel="stylesheet" href="{{ asset('/lib/material-design-iconic-font/dist/css/material-design-iconic-font.min.css') }}"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="{{ asset('/lib/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.min.css') }}"  media="screen,projection"/>
+
+    <!-- CSS -->
+    <link type="text/css" rel="stylesheet" href="{{ asset('/dist/css/app_1.min.css') }}"  media="screen,projection"/>
+</head>
+
+<body>
+<div class="login-content">
+    <!-- Login -->
+    <div class="lc-block toggled" id="l-login">
+
+        {!! Form::open(['route'=>'auth.postLogin', 'method' => "POST" ]) !!}
+            {!! csrf_field() !!}
+
+            <div class="lcb-form">
+                <div class="form-group">
+                    <div class="fg-line">
+                        {!! Form::text('username', old('username'), array('class' => 'form-control input-sm', 'placeholder' => 'Login')) !!}
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="fg-line">
+                        {!! Form::password('password', array('class' => 'form-control input-sm', 'placeholder' => 'Senha')) !!}
+                    </div>
+                </div>
+
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" value="">
+                        <i class="input-helper"></i>
+                        Manter-me conectado
+                    </label>
+                </div>
+
+                <button type="submit">Logar</button>
+            </div>
+
+        {!! Form::close() !!}
+
+        <div class="lcb-navigation">
+            <a href="" data-ma-action="login-switch" data-ma-block="#l-register"><i class="zmdi zmdi-plus"></i> <span>Cadastre-se</span></a>
+            <a href="" data-ma-action="login-switch" data-ma-block="#l-forget-password"><i>?</i> <span>Esquecei a senha</span></a>
+        </div>
+    </div>
+
+    <!-- Register -->
+    <div class="lc-block" id="l-register">
+        <div class="lcb-form">
+            <div class="form-group">
+                <div class="fg-line">
+                    <input type="text" class="form-control" placeholder="Username">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="fg-line">
+                    <input type="text" class="form-control" placeholder="Email Address">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="fg-line">
+                    <input type="password" class="form-control" placeholder="Password">
+                </div>
+            </div>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" value="">
+                    <i class="input-helper"></i>
+                    Terms and Condition
+                </label>
+            </div>
+
+            <a href="" class="btn btn-login btn-success"><i class="zmdi zmdi-check"></i></a>
+        </div>
+
+        <div class="lcb-navigation">
+            <a href="" data-ma-action="login-switch" data-ma-block="#l-login"><i class="zmdi zmdi-long-arrow-right"></i> <span>Sign in</span></a>
+            <a href="" data-ma-action="login-switch" data-ma-block="#l-forget-password"><i>?</i> <span>Forgot Password</span></a>
+        </div>
+    </div>
+
+    <!-- Forgot Password -->
+    <div class="lc-block" id="l-forget-password">
+        <div class="lcb-form">
+            <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu risus. Curabitur commodo lorem fringilla enim feugiat commodo sed ac lacus.</p>
+
+            <div class="fg-line">
+                <input type="text" class="form-control" placeholder="Email Address">
+            </div>
+
+            <a href="" class="btn btn-login btn-success"><i class="zmdi zmdi-check"></i></a>
+        </div>
+
+        <div class="lcb-navigation">
+            <a href="" data-ma-action="login-switch" data-ma-block="#l-login"><i class="zmdi zmdi-long-arrow-right"></i> <span>Sign in</span></a>
+            <a href="" data-ma-action="login-switch" data-ma-block="#l-register"><i class="zmdi zmdi-plus"></i> <span>Register</span></a>
+        </div>
+    </div>
+</div>
+
+<!-- Javascript Libraries -->
+<script src="{{ asset('/lib/jquery/dist/jquery.js') }}"></script>
+<script src="{{ asset('/lib/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('/lib/Waves/dist/waves.min.js') }}"></script>
+
+<script type="text/javascript" src={{ asset('/dist/js/app.js') }}></script>
+
+</body>
+</html>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,122 +1,111 @@
-<!DOCTYPE html>
-<!--[if IE 9 ]><html class="ie9"><![endif]-->
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Material Admin</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+    <title>MB Crédito</title>
 
-    <!--CSS Vendor-->
-    <link type="text/css" rel="stylesheet" href="{{ asset('/lib/material-design-iconic-font/dist/css/material-design-iconic-font.min.css') }}"  media="screen,projection"/>
-    <link type="text/css" rel="stylesheet" href="{{ asset('/lib/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.min.css') }}"  media="screen,projection"/>
+    <!-- -->
+    <link href="{{ asset('/lib/bootstrap/dist/css/bootstrap.css') }}" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="{{asset('/dist/css/AdminLTE.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('/dist/fonts/font-awesome/css/font-awesome.css')}}" />
 
-    <!-- CSS -->
-    <link type="text/css" rel="stylesheet" href="{{ asset('/dist/css/app_1.min.css') }}"  media="screen,projection"/>
+    <!-- Ionicons -->
+    <link href="{{ asset('/dist/css/ionicons.min.css') }}" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="{{asset('/dist/css/animate.css')}}" />
+    <link rel="stylesheet" href="{{asset('/dist/css/_all-skins.min.css')}}" />
+
+    <!-- -->
+    <link rel="stylesheet" href="{{asset('/dist/css/cs-login.css')}}" />
+    <link rel="stylesheet" href="{{asset('/dist/css/select2.min.css')}}" />
+
+    {{--<link rel="icon" type="image/x-icon" href="{{ asset('logo_sistema_menu_sbl.ico') }}" />--}}
 </head>
 
-<body>
-<div class="login-content">
-    <!-- Login -->
-    <div class="lc-block toggled" id="l-login">
+<body class="skin-red sidebar-mini wysihtml5-supported fixed" style="background-position: 119px 27px, 63px center;">
 
-        {!! Form::open(['route'=>'auth.postLogin', 'method' => "POST" ]) !!}
-            {!! csrf_field() !!}
+<div class="wrapper">
+    <div class="container">
+        <div class="row vertical-offset-100">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="panel panel-default" style="margin-top: -50px">
+                    <div class="panel-heading">
+                        <div class="row-fluid user-row">
+                            <center>
+                                <h3>CBO</h3>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <!-- alerta -->
+                        @if(Session::has('errors'))
+                            <div class="alert alert-danger">
+                                @lang('auth.failed')<br>
+                            </div>
+                            @endif
 
-            <div class="lcb-form">
-                <div class="form-group">
-                    <div class="fg-line">
-                        {!! Form::text('username', old('username'), array('class' => 'form-control input-sm', 'placeholder' => 'Login')) !!}
+                            <!-- logo -->
+                            {{--<div class="login-logo">
+                                <center><img id="logo" src="{{ asset('/dist/img/logo.jpg') }}"
+                                             class="img-responsive" alt="Logo"/></center>
+                            </div>--}}
+
+                            <!-- formulário -->
+                            {!! Form::open(['route'=>'auth.postLogin', 'method' => "POST" ]) !!}
+                            {!! csrf_field() !!}
+
+                            <div class="lcb-form">
+                                <div class="form-group">
+                                    <div class="fg-line">
+                                        {!! Form::text('username', old('username'), array('class' => 'form-control input-sm', 'placeholder' => 'Login')) !!}
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="fg-line">
+                                        {!! Form::password('password', array('class' => 'form-control input-sm', 'placeholder' => 'Senha')) !!}
+                                    </div>
+                                </div>
+
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" value="">
+                                        <i class="input-helper"></i>
+                                        Manter-me conectado
+                                    </label>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-xs-4">
+                                        <button type="submit" class="btn btn-lg btn-primary btn-block">Entrar</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {!! Form::close() !!}
+
+                            <center>
+                                <strong>
+                                    Copyright © 2004-2015
+                                    <a target="__blanck" href="http://www.serbinario.com.br/">Serbinário</a>
+                                    <br>
+                                </strong>
+                                Todos os direitos reservados.
+                            </center>
+
+                            <center>
+                                <img class="img-responsive" src="{{ asset('/dist/img/serbinario_logo.png') }}" style="margin-bottom:                                        15px; margin-top: 10px" width="200" height="100">
+                            </center>
                     </div>
                 </div>
-
-                <div class="form-group">
-                    <div class="fg-line">
-                        {!! Form::password('password', array('class' => 'form-control input-sm', 'placeholder' => 'Senha')) !!}
-                    </div>
-                </div>
-
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" value="">
-                        <i class="input-helper"></i>
-                        Manter-me conectado
-                    </label>
-                </div>
-
-                <button type="submit">Logar</button>
             </div>
-
-        {!! Form::close() !!}
-
-        <div class="lcb-navigation">
-            <a href="" data-ma-action="login-switch" data-ma-block="#l-register"><i class="zmdi zmdi-plus"></i> <span>Cadastre-se</span></a>
-            <a href="" data-ma-action="login-switch" data-ma-block="#l-forget-password"><i>?</i> <span>Esquecei a senha</span></a>
-        </div>
-    </div>
-
-    <!-- Register -->
-    <div class="lc-block" id="l-register">
-        <div class="lcb-form">
-            <div class="form-group">
-                <div class="fg-line">
-                    <input type="text" class="form-control" placeholder="Username">
-                </div>
-            </div>
-
-            <div class="form-group">
-                <div class="fg-line">
-                    <input type="text" class="form-control" placeholder="Email Address">
-                </div>
-            </div>
-
-            <div class="form-group">
-                <div class="fg-line">
-                    <input type="password" class="form-control" placeholder="Password">
-                </div>
-            </div>
-
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox" value="">
-                    <i class="input-helper"></i>
-                    Terms and Condition
-                </label>
-            </div>
-
-            <a href="" class="btn btn-login btn-success"><i class="zmdi zmdi-check"></i></a>
-        </div>
-
-        <div class="lcb-navigation">
-            <a href="" data-ma-action="login-switch" data-ma-block="#l-login"><i class="zmdi zmdi-long-arrow-right"></i> <span>Sign in</span></a>
-            <a href="" data-ma-action="login-switch" data-ma-block="#l-forget-password"><i>?</i> <span>Forgot Password</span></a>
-        </div>
-    </div>
-
-    <!-- Forgot Password -->
-    <div class="lc-block" id="l-forget-password">
-        <div class="lcb-form">
-            <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu risus. Curabitur commodo lorem fringilla enim feugiat commodo sed ac lacus.</p>
-
-            <div class="fg-line">
-                <input type="text" class="form-control" placeholder="Email Address">
-            </div>
-
-            <a href="" class="btn btn-login btn-success"><i class="zmdi zmdi-check"></i></a>
-        </div>
-
-        <div class="lcb-navigation">
-            <a href="" data-ma-action="login-switch" data-ma-block="#l-login"><i class="zmdi zmdi-long-arrow-right"></i> <span>Sign in</span></a>
-            <a href="" data-ma-action="login-switch" data-ma-block="#l-register"><i class="zmdi zmdi-plus"></i> <span>Register</span></a>
         </div>
     </div>
 </div>
+<!-- wrapper -->
 
-<!-- Javascript Libraries -->
-<script src="{{ asset('/lib/jquery/dist/jquery.js') }}"></script>
-<script src="{{ asset('/lib/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('/lib/Waves/dist/waves.min.js') }}"></script>
-
-<script type="text/javascript" src={{ asset('/dist/js/app.js') }}></script>
-
-</body>
-</html>
+<!-- bibliotecas -->
+<script type="text/javascript" src="{{ asset('/lib/jquery/dist/jquery.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/lib/bootstrap/dist/js/bootstrap.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/lib/jquery-slimscroll/jquery.slimscroll.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/lib/fastclick/lib/fastclick.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/dist/js/app_2.min.js') }}"></script>
